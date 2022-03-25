afe2664efd154b50d6728425d029b75aeac539ee
dechaud-sv <dechaud.sv@icloud.com>
2022-03-24 21:25:34 -0500

    Create Bird.h, Bird.cpp, main.cpp, and CMakeLists.txt files

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Sophia DeChaud
+// CSCE306, Spring 2022
+// Lab 10, main.cpp
+
+// Main driver for demo for lab 10
+
+#include <string>
+#include <iostream>
+using namespace std;
+#include "Bird.h"
+
+
+int main() {
+    cout << "Tweet Tweet, chirp" << endl;
+
+    return 0;
+}
+
